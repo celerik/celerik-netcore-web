6139ab84163a60cd38a00b5f431dbf73a56dce34
Juan Cuartas <equisoide@gmail.com>
2020-04-20 07:51:36 -0500

    Created base class for testing

---
+++ source/Celerik.NetCore.Web.Test/WebBaseTest.cs
@@ -0,0 +1,12 @@
+﻿using Celerik.NetCore.Util;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Celerik.NetCore.Web.Test
+{
+    public class WebBaseTest : BaseTest
+    {
+        protected override void AddServices(IServiceCollection services)
+        {
+        }
+    }
+}
