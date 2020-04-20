b3b98232b2e6ef03f9dc064dd2d4cb4ccb2c55da
Juan Cuartas <equisoide@gmail.com>
2020-04-20 07:44:45 -0500

    Created CorsConfig class

---
+++ source/Celerik.NetCore.Web/Cors/CorsConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace Celerik.NetCore.Web
+{
+    /// <summary>
+    /// Defines CORS configuration for the current environment.
+    /// </summary>
+    public class CorsConfig
+    {
+        /// <summary>
+        /// The CORS Policy to be applied.
+        /// </summary>
+        public CorsPolicy Policy { get; set; }
+
+        /// <summary>
+        /// List of Allowed Origins, when the policy is: AllowSpecificOrigins.
+        /// </summary>
+        public string[] Origins { get; set; }
+    }
+}
