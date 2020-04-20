4666e80a5a6c2dadd41fdf5148cb8c72af559738
Juan Cuartas <equisoide@gmail.com>
2020-04-20 07:45:03 -0500

    Created Cors enumerations

---
+++ source/Celerik.NetCore.Web/Cors/CorsEnums.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Celerik.NetCore.Web
+{
+    public enum CorsPolicy
+    {
+        [Description("Disabled")]
+        Disabled = 1,
+
+        [Description("AllowSpecificOrigins")]
+        AllowSpecificOrigins = 2,
+
+        [Description("AllowAnyOrigin")]
+        AllowAnyOrigin = 3
+    }
+}
