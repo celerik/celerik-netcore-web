6bde71583c74e5e57ed7da507baf0597f8b2ae41
Juan Cuartas <equisoide@gmail.com>
2020-04-20 07:49:29 -0500

    Created Swagger config file

---
+++ source/Celerik.NetCore.Web/Swagger/SwaggerConfig.cs
@@ -0,0 +1,28 @@
+﻿namespace Celerik.NetCore.Web
+{
+    /// <summary>
+    /// Defines Swagger configuration for the current environment.
+    /// </summary>
+    public class SwaggerConfig
+    {
+        /// <summary>
+        /// Indicates whether Swagger is enabled.
+        /// </summary>
+        public bool IsEnabled { get; set; }
+
+        /// <summary>
+        /// Swagger Api version.
+        /// </summary>
+        public string ApiVersion { get; set; }
+
+        /// <summary>
+        /// Swagger Api name.
+        /// </summary>
+        public string ApiName { get; set; }
+
+        /// <summary>
+        /// Swagger Json endpoint.
+        /// </summary>
+        public string JsonEndpoint { get; set; }
+    }
+}
