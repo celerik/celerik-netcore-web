eb9cea4b6f18bd54dbd010164719533bbbe5b5c3
Juan Cuartas <equisoide@gmail.com>
2020-04-20 07:48:30 -0500

    Created PermissionAttribute

---
+++ source/Celerik.NetCore.Web/Security/PermissionAttribute .cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Celerik.NetCore.Web
+{
+    /// <summary>
+    /// Custom Attribute to authorize access to an endpoint based on User Permissions.
+    /// </summary>
+    public class PermissionAttribute : TypeFilterAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        /// <param name="permissions">List of permissions that grant access to the endpoint.
+        /// E.g.: "AddUser".</param>
+        public PermissionAttribute(params string[] permissions)
+            : base(typeof(PermissionFilter))
+        {
+            Arguments = new object[] { permissions };
+        }
+    }
+}
