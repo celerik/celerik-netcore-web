9876e7c32b6b6952d10ad951f01f371cb382b7be
AzureAD\JuanDavidCuartas <jcuartas@celerik.com>
2023-03-30 14:37:46 -0500

    Added auth header logic

---
+++ source/Celerik.NetCore.Web/Filters/AuthorizationHeader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Celerik.NetCore.Web
+{
+    public class AuthorizationHeader : Attribute, IOperationFilter
+    {
+        private readonly IConfiguration _config;
+
+        public AuthorizationHeader(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            operation.Parameters.Add(new OpenApiParameter()
+            {
+                Name = "X-Authorization",
+                In = ParameterLocation.Header,
+                Required = false,
+                Schema = new OpenApiSchema() { Type = "string" },
+                Example = new OpenApiString(_config["SwaggerConfig:BearerToken"]),
+            });
+        }
+    }
+}
